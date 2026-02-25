6dfa4cf27699b1759a70bafa31d39b63dbd8998e
rastaasta <218407492+rastaasta@users.noreply.github.com>
2026-02-25 16:11:51 -0500

    Repeated the string

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let strings = "";
+    for (let i = 0; i < num; i++) {
+        strings += string;
+    }
+    return strings;
 };
+
+console.log(repeatString("hey", 3))
 
 // Do not edit below this line
 module.exports = repeatString;
