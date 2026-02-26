9cf3a72cda904f4bf85cc8cafff03d3051c1f58f
rastaasta <218407492+rastaasta@users.noreply.github.com>
2026-02-26 12:59:52 -0500

    Created a tempature conversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  convertCels = ((temp - 32) * (5/9));
+  cels = Math.round(convertCels * 10) / 10;
+  return cels; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  convertFah = (temp * (9/5) + 32);
+  fah = Math.round(convertFah * 10) / 10;
+  return fah; 
 };
+
+console.log(convertToCelsius(100));
+console.log(convertToCelsius(32));
+console.log(convertToFahrenheit(0));
+
 
 // Do not edit below this line
 module.exports = {
