1ea8e4625a1856dfc1b274841f442348ca66c0ac
rastaasta <218407492+rastaasta@users.noreply.github.com>
2026-02-26 12:32:34 -0500

    Created a way to find leap years

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isYearDivisibleByFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isYearDivisibleByFourHundred = year % 400 === 0;
 
+    if (
+        isYearDivisibleByFour &&
+        (!isCentury || isYearDivisibleByFourHundred)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+console.log(leapYears(2000));
+console.log(leapYears(1985));
+
 
 // Do not edit below this line
 module.exports = leapYears;
